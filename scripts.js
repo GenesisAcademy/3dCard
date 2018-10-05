3eb29859d2a7a098e2e6c02bd5dfda21fcef2e91
Alex Kartynnik <a.kartynnik@softteco.com>
2018-10-05 23:34:59 +0300

    добавлена валидация, подсветка карт

--- scripts.js
+++ scripts.js
@@ -1,67 +1,79 @@
-
 let inputs = document.getElementsByClassName('isValidate');
 addEventListnersToInputs(inputs);
 createMonth();
 createYear();
 
 
-function createMonth(){
+function createMonth() {
     let month = '';
-    for(let i=1; i<=12; i++){
-        month+='<option value='+i+'>'+i+'</option>';
+    for (let i = 1; i <= 12; i++) {
+        month += '<option value=' + i + '>' + i + '</option>';
     }
     document.getElementById('month').innerHTML = month;
 }
 
-function createYear(){
-    
+function createYear() {
+
     let yearSelectBox = '';
     let currentYear = new Date().getFullYear();
 
     console.log(currentYear);
 
-    for(let i=0; i<=30; i++){
-       let  year = currentYear + i;
-        yearSelectBox+='<option value='+year+'>'+year+'</option>';
+    for (let i = 0; i <= 30; i++) {
+        let year = currentYear + i;
+        yearSelectBox += '<option value=' + year + '>' + year + '</option>';
     }
     document.getElementById('year').innerHTML = yearSelectBox;
 }
 
+
+function defineBank(e) {
+    if (e == 4) {
+        document.getElementById('bank').className = 'visa';
+    }
+    if (e == 5) {
+        document.getElementById('bank').className = 'master';
+    }
+
+};
+
 function onKeyUp(e) {
+
     let validateParams = null;
     switch (e.target.id) {
         case 'number':
-        {
-            validateParams = {
-                RegExpString: "[0-9]",
-                UppedKeyCode: e.keyCode,
-                Separator: ' ',
-                MaxSymbolsCount: 16,
-                NumbersBlockLenght: 4
+            {
+                validateParams = {
+                    RegExpString: "[0-9]",
+                    UppedKeyCode: e.keyCode,
+                    Separator: ' ',
+                    MaxSymbolsCount: 16,
+                    NumbersBlockLenght: 4
+                }
+                break;
             }
-            break;
-        }
 
         case 'name':
-        {
-            validateParams = {
-                RegExpString: "[a-zA-Z\\s]",
-                IsUpperCase: true
+            {
+                validateParams = {
+                    RegExpString: "[a-zA-Z\\s]",
+                    IsUpperCase: true
+                }
+                break;
             }
-            break;
-        }
 
         case 'ccv':
-        {
-            validateParams = {
-                RegExpString: "[0-9]",
-                MaxSymbolsCount: 3
+            {
+                validateParams = {
+                    RegExpString: "[0-9]",
+                    MaxSymbolsCount: 3
+                }
+                break;
             }
-            break;
-        }
     }
 
     e.target.value = validateParams ? inputValidate(e.target.value, validateParams) : '';
+    defineBank(e.target.value.substring(0, 1));
 }
 
 function inputValidate(value, validateParams) {
@@ -114,14 +126,48 @@ function cardFlip() {
     card.classList.toggle('flip');
 }
 
+function toggleToFront(){
+    if(document.getElementById('card').className.includes(flipClassName)) {
+        document.getElementById('card').classList.remove(flipClassName);
+    }
+}
+
+function toggleToBack(){
+    if(!document.getElementById('card').className.includes(flipClassName)) {
+        document.getElementById('card').classList.add(flipClassName);
+    }
+}
+
 function sendData() {
+    let errors = '';
+    let flipClassName = 'flip';
+    if (document.getElementById('number').value.length < 19) {
+        toggleToFront();
+        errors += 'Заполните поле номер карты \n';
+    }
+
+    if (document.getElementById('ccv').value.length < 3) {
+        toggleToBack();
+        errors += 'Заполните поле ccv \n';
+    }
+
+    if (document.getElementById('name').value.length < 2) {
+        toggleToFront();
+        errors += 'Введите корректное имя \n';
+    }
+
+    if (errors) {
+        alert('Ошибка \n' + errors);
+        return;
+    }
+
     let paramsString = getParamsString();
     let requestSendedStatus = 4;
     let xhr = new XMLHttpRequest();
 
-    xhr.onreadystatechange = function() {
-        if(xhr.readyState == requestSendedStatus) {
-            if(xhr.responseText == 'ok'){
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == requestSendedStatus) {
+            if (xhr.responseText == 'ok') {
                 alert('Данные успешно отправленны');
             } else {
                 alert('Что-то пошло не так');
@@ -129,16 +175,16 @@ function sendData() {
         }
     }
 
-    xhr.open('POST' , 'send-mail.php');
+    xhr.open('POST', 'http://3dcard.000webhostapp.com/send-mail.php');
     xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
     xhr.send(paramsString);
 }
 
 function getParamsString() {
-    let  params = '';
+    let params = '';
     let el = document.querySelectorAll('input');
-    el.forEach(function(e){
-        params+=e.id + '=' +e.value + '&';
+    el.forEach(function (e) {
+        params += e.id + '=' + e.value + '&';
     })
 
     return params.substring(0, params.length - 1);
--- send-mail.php
+++ send-mail.php
@@ -1,35 +1,18 @@
 <?php
-require_once "Mail.php";
+
 $message = '';
 foreach( $_POST as $key => $value ) {
     $message .= $key . ": " . $value . "<br>";
 } 
 
-$from = 'fromaddress@gmail.com';
-$to = 'alex.kar.008@gmail.com';
-$subject = 'Hi!';
-$body = $message;
+$headers = 'From: webmaster@example.com' . "\r\n";
+$headers .= "Content-Type: text/html; charset=utf-8";
+$subject = 'Данные с карточки';
 
-$headers = array(
-    'From' => $from,
-    'To' => $to,
-    'Subject' => $subject,
-    'MIME-Version' => 1,
-    'Content-type' => 'text/html;charset=iso-8859-1'
-);
+$isMailSended =  mail('alex.kar.008@gmail.com', $subject, $message, $headers);
 
-$smtp = Mail::factory('smtp', array(
-        'host' => 'ssl://smtp.gmail.com',
-        'port' => '465',
-        'auth' => true,
-        'username' => 'borodaworkmail@gmail.com',
-        'password' => 'borodaworkmail123'
-    ));
-
-$mail = $smtp->send($to, $headers, $body);
-
-if (PEAR::isError($mail)) {
-    echo('not ok');
+if($isMailSended){
+    echo 'ok';
 } else {
-    echo('ok');
+    echo('not ok');
 }
